0fdc03affedd3a014e56cb172f6a8dbd5d860d3f
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-03-17 16:19:20 -0600

    Set up config clients

---
+++ rag_llm_energy_expert/config.py
@@ -0,0 +1,11 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class GCP_CONFIG(BaseSettings):
+    PROJECT_ID: str = "natural-reserve-454018-j9"
+    DEV_SA: SecretStr = SecretStr("mock_dev_service_account")
+    BUCKET_NAME: str = "mock_bucket_name"
+
+class LLM_CONFIG(BaseSettings):
+    OPENAI_API_KEY: SecretStr = SecretStr("mock_openai_api_key")
