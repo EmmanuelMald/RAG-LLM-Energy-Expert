32f629e036f71e9b5e618b94c5499733a289f460
Emmanuel Amador Maldonado <128644954+EmmanuelMald@users.noreply.github.com>
2025-03-17 23:38:30 -0600

    Set gcs secret manager utils (#2)
    * Authenticate to GCP
    
    * Set up config clients
    
    * Add libraries to connect to a notebook
    
    * Create dependency groups
    
    * Set up the name of the bucket to create
    
    * Command to login to GCP and sync the project dependencies
    
    * Modify gitignore to exclude data folder
    
    * Set cloud storage functions
    
    * Add command to upload all dependencies in Makefile
    
    * Add GCP secretmanager library
    
    * Modified project_id in config class
    
    * Add functions for GCP secret manager
    
    * Delete unused library in secret_manager utils

---
+++ rag_llm_energy_expert/config.py
@@ -0,0 +1,12 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class GCP_CONFIG(BaseSettings):
+    PROJECT_ID: str = "learned-stone-454021-c8"
+    DEV_SA: SecretStr = SecretStr("mock_dev_service_account")
+    BUCKET_NAME: str = "rag_llm_energy_expert"
+    REGION: str = "northamerica-south1"
+
+class LLM_CONFIG(BaseSettings):
+    OPENAI_API_KEY: SecretStr = SecretStr("mock_openai_api_key")
