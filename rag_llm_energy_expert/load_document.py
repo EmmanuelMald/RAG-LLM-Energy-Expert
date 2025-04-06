c3cd0aef8f04ee88cbd61fbf5254087fc3a95c62
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-06 14:49:28 -0600

    Parsing and indexing functions were linted and formatted

--- rag_llm_energy_expert/load_document.py
+++ rag_llm_energy_expert/load_document.py
@@ -16,11 +16,13 @@ from utils.gcp.gcs import upload_file_from_memory
 # Initialize GCP config values
 gcp_config = GCP_CONFIG()
 
-def parse_file(gcs_file_path: str, 
-               chunk_size: int = 256, 
-               chunk_overlap: int = 0,
-               bucket_name: str = gcp_config.BUCKET_NAME,
-               ) -> None:
+
+def parse_file(
+    gcs_file_path: str,
+    chunk_size: int = 256,
+    chunk_overlap: int = 0,
+    bucket_name: str = gcp_config.BUCKET_NAME,
+) -> None:
     """
     Parse a file to be embedded into vectors.
 
@@ -34,50 +36,60 @@ def parse_file(gcs_file_path: str,
         None
     """
     logger.info("Parsing file...")
-    allowed_formats = {
-        "pdf": extract_pdf_content
-        }
+    allowed_formats = {"pdf": extract_pdf_content}
 
     if not isinstance(gcs_file_path, str):
-        raise ValueError("gcs_file_path parameter must be a string. Ex: 'folder/my_doc.pdf'")
-    
+        raise ValueError(
+            "gcs_file_path parameter must be a string. Ex: 'folder/my_doc.pdf'"
+        )
+
     extension = gcs_file_path.split(".")[-1]
 
-    if not extension in allowed_formats:
-        raise ValueError(f"The file is in the format {extension}, which cannot be processed. Current allowed formats are: {allowed_formats.keys()}")
-    
+    if extension not in allowed_formats:
+        raise ValueError(
+            f"The file is in the format {extension}, which cannot be processed. Current allowed formats are: {allowed_formats.keys()}"
+        )
+
     # Step 1: Extract the data and save it into a dictionary
-    file_data = allowed_formats[extension](gcs_file_path = gcs_file_path, bucket_name = bucket_name)
+    file_data = allowed_formats[extension](
+        gcs_file_path=gcs_file_path, bucket_name=bucket_name
+    )
 
     # Step 2:  Chunk the data by the markdown headers
-    md_headers_chunks = chunk_by_md_headers(md_text = file_data["text"])
+    md_headers_chunks = chunk_by_md_headers(md_text=file_data["text"])
 
     # Step 3: Chunk the md_headers_chunks into smaller ones based on the chunk_size and overlap params
-    chunks_sized = size_md_chunks(md_headers_chunks = md_headers_chunks, chunk_size = chunk_size, chunk_overlap = chunk_overlap)
+    chunks_sized = size_md_chunks(
+        md_headers_chunks=md_headers_chunks,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
 
     # Add the chunk_size to the metadata
     file_data["chunk_size"] = chunk_size
 
     # Step 4: Prepare the chunks to be embedded.
-    # Returns a dictionary which each entry is a chunk 
-    chunks_to_embed = prepare_chunks_for_embeddings(chunks_sized=chunks_sized, file_data=file_data)
+    # Returns a dictionary which each entry is a chunk
+    chunks_to_embed = prepare_chunks_for_embeddings(
+        chunks_sized=chunks_sized, file_data=file_data
+    )
 
     # Step 5: Store chunks into GCP
     logger.info("Storing chunks in GCP...")
     upload_date = chunks_to_embed[0]["upload_date"]
     storage_file_path = f"chunks/{file_data['title']}_{chunk_size}_{upload_date}.txt"
-    
+
     chunks_to_store = {f"chunk{i}": chunk for i, chunk in enumerate(chunks_to_embed)}
 
     # ensure_ascii == False to preserve word's accents
     json_chunks_plain = json.dumps(chunks_to_store, ensure_ascii=False)
 
     upload_file_from_memory(
-        blob_name = storage_file_path, 
-        string_data = json_chunks_plain, 
-        bucket_name = bucket_name,
-        )
+        blob_name=storage_file_path,
+        string_data=json_chunks_plain,
+        bucket_name=bucket_name,
+    )
 
     logger.info(f"Chunks stored in {storage_file_path}")
-    
+
     return
