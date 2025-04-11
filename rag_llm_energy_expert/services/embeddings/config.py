40d9f2052a5c72b8c9ee3164654a7cad8ea329f5
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-11 12:39:58 -0600

    Modify the name of the embeddings class, now uses the PascalCase format name

--- rag_llm_energy_expert/services/embeddings/config.py
+++ rag_llm_energy_expert/services/embeddings/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings
 
 
-class EMBEDDINGS_CONFIG(BaseSettings):
+class EmbeddingsConfig(BaseSettings):
     CHUNK_OVERLAP: int = 10
     EMBEDDING_MODEL: str = "nomic-ai/nomic-embed-text-v2-moe"
