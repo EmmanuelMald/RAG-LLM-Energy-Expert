8562ac27b73c3c9b571c03b73c66ca7122bce0d2
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-11 19:08:26 -0600

    Fix embeddings API errors related to validation messages

--- rag_llm_energy_expert/services/embeddings/app/main.py
+++ rag_llm_energy_expert/services/embeddings/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from models import EmbeddingRequest, EmbeddingResponse, Chunk
+from fastapi import FastAPI, HTTPException
+from models import EmbeddingRequest, EmbeddingResponse, Chunk, Payload
+from loguru import logger
 
 import sys
 
@@ -12,22 +13,30 @@ app = FastAPI()
 
 @app.post("/embed-text", response_model=EmbeddingResponse)
 def generate_embeddings(request: EmbeddingRequest):
-    raw_chunks = text_embedder(
-        text=request.text,
-        chunk_overlap=request.chunk_overlap,
-        embedding_model_name=request.embedding_model_name,
-        metadata=request.metadata,
-    )
-
-    list_of_chunks = [
-        Chunk(
-            vector_id=chunk["vector_id"],
-            vector=chunk["vector"],
-            metadata=chunk["metadata"],
+    try:
+        raw_chunks = text_embedder(
+            text=request.text,
+            chunk_overlap=request.chunk_overlap,
+            embedding_model_name=request.embedding_model_name,
+            metadata=request.metadata,
         )
-        for chunk in raw_chunks
-    ]
 
-    response = EmbeddingResponse(list_of_chunks)
+        list_of_chunks = [
+            Chunk(
+                vector_id=chunk["id"],
+                vector=chunk["vector"],
+                payload=Payload(
+                    text=chunk["payload"]["text"],
+                    metadata=chunk["payload"]["metadata"],
+                ),
+            )
+            for chunk in raw_chunks
+        ]
+
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+    response = EmbeddingResponse(chunks=list_of_chunks)
 
     return response
