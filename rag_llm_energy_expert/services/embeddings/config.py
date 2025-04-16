30a235062c2872090d2a7e0ffcd88ba968f49e20
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-15 19:00:27 -0600

    Change the embedding model used in the embedding service to allow documents in multiple languages

--- rag_llm_energy_expert/services/embeddings/config.py
+++ rag_llm_energy_expert/services/embeddings/config.py
@@ -3,4 +3,4 @@ from pydantic_settings import BaseSettings
 
 class EmbeddingsConfig(BaseSettings):
     CHUNK_OVERLAP: int = 10
-    EMBEDDING_MODEL: str = "sentence-transformers/all-MiniLM-L6-v2"
+    EMBEDDING_MODEL: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
