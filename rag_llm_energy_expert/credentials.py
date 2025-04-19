d0f209acbca62c608412c4ed0f2b9b26943cbbdd
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-18 21:21:10 -0600

    In credentials.py, start a Client before executing any function to optimize load times

--- rag_llm_energy_expert/credentials.py
+++ rag_llm_energy_expert/credentials.py
@@ -8,6 +8,7 @@ from rag_llm_energy_expert.utils.gcp.secret_manager import get_secret
 
 gcp_config = GCPConfig()
 llm_config = LLMConfig()
+qdrant_config = QdrantConfig()
 
 
 def get_qdrant_config() -> QdrantConfig:
@@ -20,11 +21,10 @@ def get_qdrant_config() -> QdrantConfig:
     Return:
         QdrantConfig instance
     """
-    qdrant = QdrantConfig()
 
     # Get secret id and version id
-    secret_id = qdrant.SECRET_ID
-    version_id = qdrant.VERSION_ID
+    secret_id = qdrant_config.SECRET_ID
+    version_id = qdrant_config.VERSION_ID
 
     # Get api_key
     api_key = get_secret(secret_id, version_id, gcp_config.PROJECT_ID)
