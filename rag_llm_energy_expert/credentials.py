09fabf31335964ab81f48cc882b37de1a06e7864
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-04-18 19:16:10 -0600

    Add an LLMConfig class

--- rag_llm_energy_expert/credentials.py
+++ rag_llm_energy_expert/credentials.py
@@ -3,10 +3,11 @@ import sys
 
 sys.path.append("..")
 
-from rag_llm_energy_expert.config import QdrantConfig, GCPConfig
+from rag_llm_energy_expert.config import QdrantConfig, GCPConfig, LLMConfig
 from rag_llm_energy_expert.utils.gcp.secret_manager import get_secret
 
 gcp_config = GCPConfig()
+llm_config = LLMConfig()
 
 
 def get_qdrant_config() -> QdrantConfig:
@@ -59,3 +60,14 @@ def get_gcp_config() -> GCPConfig:
     embedding_service_id_token = generate_id_token(embedding_service_audience)
 
     return GCPConfig(EMBEDDING_SERVICE_IDTOKEN=embedding_service_id_token)
+
+
+def get_llm_config() -> LLMConfig:
+    """
+    Get the LLMConfig with secret info
+    """
+    secret_id = llm_config.SECRET_ID
+    version_id = llm_config.SECRET_VERSION
+    api_key = get_secret(secret_id, version_id, gcp_config.PROJECT_ID)
+
+    return LLMConfig(API_KEY=api_key)
